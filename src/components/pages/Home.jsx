4841e5efa0eebf64289b4117f0d4c0fc4b5e8cf3
unknown <messiasbento944@gmail.com>
2023-08-03 10:37:40 -0300

    feat: Pagina home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styles from './Home.module.css';
+
+//imagem
+import savings from '../../assets/img/savings.svg';
+import LinkButton from '../layout/LinkButton';
 
 function Home(){
     return(
-        <>
-          <h1>Home</h1>
-        </>
+        <section className={styles.home_container} >
+          <h1>
+            Bem-Vindo ao <span>Costs</span>
+          </h1>
+          <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+          <LinkButton to="/newproject" text="Criar Projeto" />
+          <img src={savings} alt="Costs" />
+        </section>
     );
 }
 
