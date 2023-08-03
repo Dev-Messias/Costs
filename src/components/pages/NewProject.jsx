c22843fab0685b8fd83454c06bc3cce84c972d66
unknown <messiasbento944@gmail.com>
2023-08-03 17:44:59 -0300

    inicio inserindo dados

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from './NewProject.module.css'
-
+import { useHistory } from 'react-router-dom'
 import ProjectForm from '../project/ProjectForm';
 
 function NewProject(){
+    const history = useHistory();
+
+    function createPost(project){
+      
+    }
+
     return(
         <div className={styles.newproject_container} >
           <h1>Criar projeto</h1>
